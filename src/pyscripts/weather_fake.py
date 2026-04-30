9acc1146ad2e95dbc62b3e5d3cc08a46169de5cb
Hugo Puig <hugopuig156@gmail.com>
2026-04-30 16:11:20 +0200

    Modifs de Hugo puig

--- src/pyscripts/weather_fake.py
+++ src/pyscripts/weather_fake.py
@@ -1,9 +1,10 @@
 """Donnees meteo fictives pour les exercices."""
 
 FAKE_WEATHER = {
-    "toulouse": "soleil",
-    "montpellier": "vent",
-    "paris": "nuages",
+    "pezilla": "soleil",
+    "pezilla": "vent",
+    "pezilla": "nuages",
+    "pezilla": "tramontane",
 }
 
 
